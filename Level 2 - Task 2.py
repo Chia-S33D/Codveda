ab7d511ff4820f49b3a54ba28d5890aae42d3048
Chiamaka Chijioke-Agina <cagina.chi@gmail.com>
2026-06-11 16:22:38 +0200

    Created L2 Task 2 file

---
+++ Level 2 - Task 2.py
@@ -0,0 +1,6 @@
+# 02 June 2026
+# Task 2: Time Series Analysis
+# Needed: Python, Pandas, Matplotlib, Statsmodels
+# Aim: Plot time series, decompose into trend/seasonality/residuals, apply moving averages
+
+
